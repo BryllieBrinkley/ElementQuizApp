5c2758900c4975afb54ca3cf079b075642193245
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-05-29 11:45:52 -0400

    setup button functions to handle user interaction

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+        
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        
+        updateElement()
+    }
+    
+    
+    
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateElement()
     }
 
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        answerLabel.text = "?"
+    }
+    
 
 }
 
