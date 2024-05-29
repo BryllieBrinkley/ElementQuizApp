2836953cf1ba8d2b7cb45d9c2916b7a1063e0091
JIbryll Brinkley <49615544+BryllieBrinkley@users.noreply.github.com>
2024-05-29 12:31:49 -0400

    setup falshcard and quiz modes, setup single path UI pattern

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,42 +7,76 @@
 
 import UIKit
 
+enum Mode {
+    case flashCard, quiz
+}
+
+enum State {
+    case question, answer
+}
+
+
+var mode: Mode = .flashCard
+var state: State = .question
+
 class ViewController: UIViewController {
     
     let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
 
     @IBAction func showAnswer(_ sender: Any) {
-        answerLabel.text = elementList[currentElementIndex]
+        state = .answer
         
+        updateUI()
     }
     
     @IBAction func next(_ sender: Any) {
+        
         currentElementIndex += 1
         if currentElementIndex >= elementList.count {
             currentElementIndex = 0
         }
+        state = .question
         
-        updateElement()
+        updateUI()
     }
-    
-    
     
     var currentElementIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateElement()
+        updateUI()
     }
 
-    func updateElement() {
+    func updateFlashCardUI() {
         let elementName = elementList[currentElementIndex]
         let image = UIImage(named: elementName)
         imageView.image = image
         
-        answerLabel.text = "?"
+        if state == .answer {
+            answerLabel.text = elementName
+        } else {
+            answerLabel.text = "?"
+        }
+    }
+    
+    func updateQuizUI() {
+        
+    }
+    
+    
+    func updateUI() {
+        switch mode {
+        case .flashCard:
+            updateFlashCardUI()
+        case .quiz:
+            updateQuizUI()
+        }
     }
     
 
